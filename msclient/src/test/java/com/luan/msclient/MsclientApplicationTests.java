b11c56d7ce8716ebc7f65d559dd7fb7f7468d1f7
Luan Pinheiro <lluanps@gmail.com>
2023-03-15 04:52:05 -0300

    Microservise Client criado

---
+++ msclient/src/test/java/com/luan/msclient/MsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
