9eca03fcdb09fdded73e8b82c8c7ab2ded9392c9
Luan Pinheiro <lluanps@gmail.com>
2023-03-15 17:29:48 -0300

    Configurando o microservice de Cartões

---
+++ msCartoes/src/main/java/com/luan/mscartoes/MsCartoesApplication.java
@@ -0,0 +1,15 @@
+package com.luan.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsCartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCartoesApplication.class, args);
+	}
+
+}
---
+++ msCartoes/src/main/java/com/luan/mscartoes/application/CartoesResource.java
@@ -0,0 +1,18 @@
+package com.luan.mscartoes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cartoes")
+public class CartoesResource {
+	
+	@GetMapping
+	public String status() {
+		return "ok";
+	}
+	
+	
+
+}
